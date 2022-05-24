394433effcf9fe0ba289c3ff9a1de17c6e9fad18
Alberto Márquez <50601838+betoml00@users.noreply.github.com>
2022-05-24 16:41:14 -0500

    código de seguidor y evadir obstáculos

--- general.ino
+++ general.ino
@@ -22,8 +22,8 @@ SharpDistSensor sensorIR(irPin, medianFilterWindowSize);
 long duracion, distancia;
 
 // Motores
-int velIzq = 85; // 127
-int velDer = 78; // Anterior: 127, 63, 85, 116, 100, 92, 88
+int velIzq = 100; // 127
+int velDer = 90; // Anterior: 127, 63, 85, 116, 100, 92, 88
 
 // Sensores de luz
 long valDer;
@@ -54,26 +54,71 @@ void setup() {
 }
 
 void loop() {
-// Lectura de sensores
-//  long distU_Der = leeDistU_Der();  
-//  long distU_Izq = leeDistU_Izq();
-//  Serial.println("Ult. izq.: " + String(distU_Izq));
-//  Serial.println("Ult. der.: " + String(distU_Der));   
-//  Serial.println("Infrarrojo: " + String(distIR));
-  unsigned int distIR = sensorIR.getDist();
 
+  unsigned int distIR = sensorIR.getDist();
+  long distU_Der = leeDistU_Der();  
+  long distU_Izq = leeDistU_Izq();
+  //Serial.println("Ult. izq.: " + String(distU_Izq));
+  //Serial.println("Ult. der.: " + String(distU_Der));   
+  //Serial.println("Infrarrojo: " + String(distIR));  
   valDer = analogRead(luzDer);
   valIzq = analogRead(luzIzq);
-  dif = (valIzq-valDer-15)*0.75;
-  analogWrite(enblIzq, velIzq-dif);
-  analogWrite(enblDer, velDer+dif);
-  Serial.println(String(valIzq) + ";" + String(valDer) + ";" + String(dif));  
-  adelante(50);
-  delay(50);
+  dif = (valIzq-valDer-15)*0.6;
+  long valMinLuz = 300;
+  long valMaxLuz = 700;
+  long valMinIR = 150;
+  long valMinDist = 5;
 
-  if (distIR < 100) {
+  Serial.println("ValDer:" + String(valDer));
+  
+  if (valDer > valMaxLuz && valIzq > valMaxLuz) {
     detener(2000000);
   }
+  else if (distIR < valMinIR) {
+    //Serial.println("Infrarrojo detectó: " + String(distIR));
+    atras(200);
+    if (distU_Der > distU_Izq) {
+      //Serial.println("US der. detectó más que el izq.: " + String(distU_Der));
+      derecha(200);      
+    } else {
+      //Serial.println("US izq. detectó más que el der.: " + String(distU_Izq));
+      izquierda(200);
+    }
+    adelante(200);
+  } else if (distU_Der < valMinDist) {
+    Serial.println("US der. detectó: " + String(distU_Der));
+    izquierda(200);
+  } else if (distU_Izq < valMinDist) {
+    Serial.println("US izq. detectó: " + String(distU_Izq));
+    derecha(200);
+  } else {
+    //Serial.println("No detectó nada");
+    if(valDer - 15 > valMinLuz || valIzq > valMinLuz) {
+      //Serial.println("Detectó luz");
+      if(dif>0){
+        if(velDer+dif>180){
+          analogWrite(enblDer, 180);
+        }
+        else{
+          analogWrite(enblDer, velDer+dif);
+        }
+        analogWrite(enblIzq, velIzq);
+      }
+      else{
+        if(velIzq-dif>180){
+          analogWrite(enblIzq, 180);
+        }
+        else{
+          analogWrite(enblIzq, velIzq-dif*1.2);
+        }
+        analogWrite(enblDer, velDer+dif*1.2);
+      }
+      adelante(50);
+    } else {
+      //Serial.println("No detectó luz: " + String(valIzq) + ";" + String(valDer) + ";" + String(dif));       
+      derecha(50);      
+    }
+  }      
 }
 
 // Función para leer el valor del sensor ultrasónico derecho
@@ -125,7 +170,7 @@ void adelante(int tiempo){
   // Motor derecho hacia adelante
   digitalWrite(motDerA, LOW);
   digitalWrite(motDerB, HIGH);
-  Serial.println("Avanza hacia adelante");
+  //Serial.println("Avanza hacia adelante");
   delay(tiempo);  
 }
 
@@ -137,7 +182,7 @@ void derecha(int tiempo) {
   // Motor derecho hacia adelante
   digitalWrite(motDerA, LOW);
   digitalWrite(motDerB, HIGH);
-  Serial.println("Gira hacia la derecha");
+  //Serial.println("Gira hacia la derecha");
   delay(tiempo);  
 }
 
